b51acf08ce4921e5a2cf786bae3f8ef248f4472d
Rithish Reddy <rithishreddy2812@gmail.com>
2022-08-16 16:45:21 +0530

    changes made

--- controllers/products.controller.js
+++ controllers/products.controller.js
@@ -16,7 +16,7 @@ async function getProductsById(req, res, next) {
   try {
     const id = req.params.id;
 
-    const product = await prisma.products.find({
+    const product = await prisma.products.findUnique({
       where: {
         id: Number(id),
       },
