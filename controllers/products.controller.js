e713557afd7b45effa0df86c8704b4042d53c10d
Rithish Reddy <rithishreddy2812@gmail.com>
2022-08-17 00:35:55 +0530

    changes made

--- controllers/products.controller.js
+++ controllers/products.controller.js
@@ -32,21 +32,6 @@ async function createProduct(req, res, next) {
   try {
     const { name, description, image, category, quantity, price, variants } =
       req.body;
-
-    // const productExists = await prisma.products.findUnique({
-    //   where: {
-    //     name,
-    //   },
-    //   select: {
-    //     name,
-    //   },
-    // });
-    // if(productExists){
-    //   return res.status(400).json({
-    //       msg:"Product Exists"
-    //   })
-    // }
-
     const newProduct = await prisma.products.create({
       data: {
         name,
@@ -95,13 +80,19 @@ async function createProductReview(req, res, next) {
         product_id: Number(id),
       },
     });
+    let reviews= await prisma.ratings.findMany({
+      where : {
+          product_id : Number(id)
+      }
+  })
 
-    res.json(newReview);
+    res.json(reviews);
   } catch (err) {
     console.error("Error while getting Products", err.message);
     next(err);
   }
 }
+
 
 module.exports = {
   getAllProducts,
